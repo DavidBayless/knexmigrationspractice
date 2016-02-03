730509448b541de3c12dea5fd2fce19922fd4fe3
David Bayless <David.C.Bayless15@gmail.com>
2016-02-02 17:58:22 -0700

    added seperate knex config file and added route for database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
+function pokedex() {
+  return knex('pokedex');
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  pokedex().select()
+  .then(function(pokemon) {
+    console.log(pokemon);
+    res.json(pokemon);
+  });
 });
 
 module.exports = router;
